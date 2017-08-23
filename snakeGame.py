1201afd2c476e7dd79ccb6791d333a59e4fb3f7d
ritikamotwani <iit2015096@iiita.ac.in>
2017-08-24 02:47:45 +0530

    FPS controller added

--- snakeGame.py
+++ snakeGame.py
@@ -14,4 +14,20 @@ else:
 #Play Surface
 playSurface = pygame.display.set_mode((720, 460))
 pygame.display.set_caption('Snake Game')
-time.sleep(5)
+
+
+#Colors
+red = pygame.Color(255, 0, 0)
+green = pygame.Color(0, 255, 0)
+black = pygame.Color(0, 0, 0)
+white = pygame.Color(255, 255, 255)
+brown = pygame.Color(165, 42, 42)
+
+
+
+# FPS controller(Frame Per Second)
+fpsController = pygame.time.Clock()
+
+#Important Variables
+snakePos = [100,50]
+snakeBody = [[100,50][90,50][80,50]]
